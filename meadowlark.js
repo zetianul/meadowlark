c56c149680a9350b7f41297e29bb7527cfbb743c
zetianul <ul_sniper@foxmail.com>
2017-09-27 18:11:17 +0800

    add test function

--- meadowlark.js
+++ meadowlark.js
@@ -9,7 +9,10 @@ app.set('view engine','handlebars')
 
 app.set('port',process.env.PORT || 3000);
 
-
+app.use(function (req,res,next) {
+    res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
+    next()
+})
 
 app.get('/',function (req,res) {
     res.render('home')
