a954a6b080c3a7d1c22d5ca2ce118c06984945ed
zetianul <ul_sniper@foxmail.com>
2017-09-30 14:53:38 +0800

    chapter 5 end

--- meadowlark.js
+++ meadowlark.js
@@ -18,8 +18,19 @@ app.get('/',function (req,res) {
     res.render('home')
 })
 
+app.get('/tours/hood-river',function (req,res) {
+    res.render('tours/hood-river')
+})
+
+app.get('/tours/request-group-rate',function (req,res) {
+    res.render('tours/request-group-rate')
+})
+
 app.get('/about',function (req,res) {
-    res.render('about',{fortune:fortune.getFortune()})
+    res.render('about',{
+        fortune:fortune.getFortune(),
+        pageTestScript:'/qa/tests-about.js'
+    })
 })
 
 //404
